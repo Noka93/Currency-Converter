95c6367d20f7bcb2cd17cd2abd17198ec4935998
Eremidious <enefolaremidious@gmail.com>
2022-04-14 13:56:29 +0100

    started js

---
+++ index.js
@@ -0,0 +1,18 @@
+const amount = document.getElementById("Amount").value;
+const currency1 = document.getElementById("currency1").value;
+const currency2 = document.getElementById("currency2").value;
+
+const convert = document.getElementById("convert").value;
+const message = document.getElementById("message").value;
+
+const botton = document.getElementById("btn").value;
+
+currency1.addEventListener('change', conversion).value;
+currency2.addEventListener('change', conversion).value;
+amount.addEventListener('input', conversion).value;
+convert.addEventListener('input', conversion).value;
+message.addEventListener('input', conversion).value;
+
+function conversion(){
+    
+}
